fc09ec91242c6f6d917f349b95566beb99e9badb
Igor Rodrigues <igor.ti.1983@gmail.com>
2025-05-19 22:18:46 +0100

    Refinamento do controller

--- src/main/java/br/com/viasoft/igormailservice/controller/MailController.java
+++ src/main/java/br/com/viasoft/igormailservice/controller/MailController.java
@@ -4,10 +4,12 @@ import br.com.viasoft.igormailservice.business.IEmailBusiness;
 import br.com.viasoft.igormailservice.core.exceptions.BusinessException;
 import br.com.viasoft.igormailservice.dto.RequestMailDTO;
 import jakarta.validation.Valid;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Log4j2
 @RestController
 @RequestMapping(path = "/api/receivemail")
 @CrossOrigin(origins = "*")
@@ -21,14 +23,16 @@ public class MailController {
     }
 
     @PostMapping
-    public Object receive(@Valid @RequestBody RequestMailDTO inputInfo) {
+    public ResponseEntity<String> receive(@Valid @RequestBody RequestMailDTO inputInfo) {
         try {
             emailBusiness.receiveRequest(inputInfo);
             return ResponseEntity.status(204).body("Envio de e-mail realizado com sucesso.");
         } catch (BusinessException ex) {
-            return ResponseEntity.status(500).body(ex);
+            log.error(ex.getMessage(), ex);
+            return ResponseEntity.status(500).body(ex.getMessage());
         } catch (Exception ex) {
-            return ResponseEntity.status(400).body(ex);
+            log.error(ex.getMessage(), ex);
+            return ResponseEntity.status(400).body(ex.getMessage());
         }
     }
 }
